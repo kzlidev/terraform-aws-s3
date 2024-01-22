963ec38ec95c0ea3ee0e6c04fb293ba97666e270
kz.li <kz.li@hashicorp.com>
2024-01-22 12:45:19 +0800

    Initial commit of S3 module

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  type        = string
+  description = "Name of S3 bucket"
+}
+
+variable "tags" {
+  description = "Tags to apply on the S3 bucket"
+  type        = map(string)
+  default     = {}
+}

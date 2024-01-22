001c24dd0d8bc979bddf24357fe5d803ea4822a3
kz.li <kz.li@hashicorp.com>
2024-01-22 13:30:52 +0800

    Updated module to enable encryption

--- s3.tf
+++ s3.tf
@@ -1,4 +1,14 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
   tags   = var.tags
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "encrypt_config" {
+  bucket = aws_s3_bucket.bucket.bucket
+
+   rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
+    }
+  }
 }
